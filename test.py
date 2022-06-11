d4bde4182e64e93837d0a0b27b10b20bb8692984
TylerSeiford <50031864+TylerSeiford@users.noreply.github.com>
2022-06-11 11:41:41 -0500

    test

--- test.py
+++ test.py
@@ -8,19 +8,30 @@ from models import Vector, GameElementState, RobotState, GameState, Controls, Ga
 FPS: float = 100
 
 
-def hood_control(robot_position: Vector, hood_rotation: Vector,
-        control_input: Controls) -> Controls:
+def hood_control(robot: RobotState, control_input: Controls) -> Controls:
     HOOD_ANGLES = [
-        180, 170, 162, 155, 147, 140, 132, 126, 120, 115,
-        110, 105, 100, 95, 90, 85, 80, 75, 70, 66,
-        62, 58, 54, 50, 47, 44, 41, 38, 36, 34,
+        180, 170, 162, 155, 147,
+        140, 132, 126, 120, 115,
+        110, 105, 100, 95, 90,
+        85, 80, 75, 70, 66,
+        62, 58, 54, 50, 47,
+        44, 41, 38, 36, 34,
         32
     ]
-    distance = math.hypot(robot_position.x, robot_position.z)
+
+    distance = math.hypot(robot.body.global_position.x,
+            robot.body.global_position.z)
+
     hood_index = int((distance - 1.3) * 10)
-    hood_index = max(min(hood_index, 30), 0)
+    hood_index = min(max(hood_index, 0), 30)
     target_hood_angle = HOOD_ANGLES[hood_index]
-    hood_angle = hood_rotation.x
+
+    hood_angle = robot.hood.local_rotation.x
+    if hood_angle >= 270:
+        hood_angle = (hood_angle - 450) * -1
+    elif hood_angle <= 90:
+        hood_angle = (hood_angle - 90) * -1
+
     if hood_angle > target_hood_angle:
         control_input.aim_up = False
         control_input.aim_down = True
@@ -47,6 +58,6 @@ if __name__ == '__main__':
             except ValueError:
                 continue # Error reading file, try again
             controls = gamepad.read()
-            controls = hood_control(robot_state.body.position, robot_state.hood.rotation, controls)
+            controls = hood_control(robot_state, controls)
             controls.write()
         time.sleep(max((1 / FPS) - (time.time() - start), 0))
